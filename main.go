5cea385f903c7b1aa62a98f7aced11aa82533864
Dylan McCormick <dylanm.bsa@gmail.com>
2025-11-07 16:50:47 -0500

    quick sesh

--- main.go
+++ main.go
@@ -7,14 +7,38 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/yuin/goldmark"
 	"github.com/yuin/goldmark/parser"
 	"go.abhg.dev/goldmark/frontmatter"
 )
 
+var postsStore = make(map[string]MarkdownPost)
+
+func loadPosts() {
+	files, err := filepath.Glob("./posts/*")
+	if err != nil {
+		log.Fatal("poop you couldn't load any files. Skill issue")
+	}
+	for _, post := range files {
+		fileName := filepath.Base(post)
+		slug := strings.TrimSuffix(fileName, ".md")
+		file, err := os.ReadFile(post)
+		if err != nil {
+			log.Fatal(err)
+		}
+		markdown := MarkdownToHtml(string(file))
+
+		postsStore[slug] = markdown
+	}
+}
+
 func main() {
+	loadPosts()
 	http.HandleFunc("/hello", handler)
+	http.HandleFunc("/blog/{slug}", blogHandler)
 	fmt.Println("Listening on :3000")
 	http.ListenAndServe(":3000", nil)
 }
@@ -24,6 +48,40 @@ type MarkdownPost struct {
 	OriginalDate string `yaml:"original_date"`
 	LastUpdated  string `yaml:"last_updated"`
 	Body         template.HTML
+}
+
+func blogHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("slug: %s", r.PathValue("slug"))
+	slug := r.PathValue("slug")
+	mdPost, ok := postsStore[slug]
+	if !ok {
+		fmt.Println("post not found")
+		http.NotFound(w, r)
+		return
+	}
+	const tpl = `
+	<!DOCTYPE html>
+	<html>
+		<title>
+			{{.Title}}
+		</title>
+		<body>
+			<div>
+				{{.Body}}
+			<div>
+			<div>
+				Last updated:{{.LastUpdated}}
+			</div>
+		</body>
+	</html>
+	`
+
+	t, err := template.New("blogpage").Parse(tpl)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	err = t.Execute(w, mdPost)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
---
+++ posts/goodbye-world.md
@@ -0,0 +1,14 @@
+---
+title: Goodbye World
+file: goodbye-world.md
+original_date: 11/5/25
+last_updated: 11/5/25
+---
+
+# Test markdown file!
+
+## This is a markdown file 
+
+THIS IS NOT A SUICIDE NOTE
+
+Lorem Ipsum et Dolorum est???
